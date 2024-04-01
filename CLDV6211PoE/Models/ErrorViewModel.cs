6415947975cec9d35f7a5eefd6cd38698e16e2ae
Slade <st10405782@vcconnect.edu.za>
2024-04-01 22:58:37 +0200

    added files to rep

---
+++ CLDV6211PoE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDV6211PoE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
